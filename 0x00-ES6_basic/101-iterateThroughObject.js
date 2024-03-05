b6eec11c3a783aeb21e684678f837982b5c9c7eb
fahrenh8T <raymondrayn98@gmail.com>
2024-03-05 05:44:13 +0300

    iterate through object: 101-iterateThroughObject.js

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,9 @@
+export default function iterateThroughObject(reportWithIterator)
+{
+    let employees = '';
+    for (const item of reportWithIterator)
+    {
+        employees += `${item} | `;
+    }
+    return employees.slice(0, -3);
+}
