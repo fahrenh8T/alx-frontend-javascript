ed2cc6efc89eb4ab6766f85b4b0c93df5375ca91
fahrenh8T <raymondrayn98@gmail.com>
2024-03-08 22:11:26 +0300

    3-currency.js fiile updt

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -3,25 +3,24 @@ export default class Currency {
       this._code = code;
       this._name = name;
     }
-  
+
     get code() {
       return this._code;
     }
-  
+
     set code(code) {
       this._code = code;
     }
-  
+
     get name() {
       return this._name;
     }
-  
+
     set name(name) {
       this._name = name;
     }
-  
+
     displayFullCurrency() {
       return `${this._name} (${this._code})`;
     }
   }
-  
