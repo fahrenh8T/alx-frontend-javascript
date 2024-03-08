6aec6a3219cbe8bfd7fef531df57b0078251b87b
fahrenh8T <raymondrayn98@gmail.com>
2024-03-08 22:11:01 +0300

    2-hbtn_course.js file updt

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -7,33 +7,33 @@ export default class HolbertonCourse {
       this._length = length;
       this._students = students;
     }
-  
+
     get name() {
       return this._name;
     }
-  
+
     set name(value) {
       if (typeof value !== 'string') {
         throw new Error('Invalid input type');
       }
       this._name = value;
     }
-  
+
     get length() {
       return this._length;
     }
-  
+
     set length(value) {
       if (typeof value !== 'number') {
         throw new Error('Invalid input type');
       }
       this._length = value;
     }
-  
+
     get students() {
       return this._students;
     }
-  
+
     set students(value) {
       if (!Array.isArray(value)) {
         throw new Error('Invalid input type');
@@ -41,4 +41,3 @@ export default class HolbertonCourse {
       this._students = value;
     }
   }
-  
